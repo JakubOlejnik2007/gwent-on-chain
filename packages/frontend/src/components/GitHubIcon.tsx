c2fa58e33640ba6731cc408f07b5aaddcbd13179
JakubOlejnik2007 <jacobole2000@gmail.com>
2024-07-16 16:40:45 +0200

    Change in layout of GitHubIcons in footer

--- packages/frontend/src/components/GitHubIcon.tsx
+++ packages/frontend/src/components/GitHubIcon.tsx
@@ -2,33 +2,34 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import React from "react";
 
+type SingleIconProps = {
+  githubUrl: string;
+  githubName: string;
+};
+
+const SingleIcon = ({githubUrl, githubName}: SingleIconProps) => {
+  return (
+    <a
+        href={githubUrl}
+        rel="noreferrer"
+        target="_blank"
+        className="flex items-center flex-col gap-3"
+      >
+        <FontAwesomeIcon
+          className="w-10 h-10 mx-3 text-zinc-500"
+          icon={faGithub}
+        />
+        <p className="h-fit">{githubName}</p>
+      </a>
+  );
+}
+
 const GitHubIcon = () => {
   return (
-    <div className="flex justify-center w-full max-w-2xl gap-5 text-center">
-      <a
-        href="https://github.com/JakubOlejnik2007"
-        rel="noreferrer"
-        target="_blank"
-        className="flex flex-col gap-3"
-      >
-        <FontAwesomeIcon
-          className="w-10 h-10 mx-3 text-zinc-500"
-          icon={faGithub}
-        />
-        <p className="h-fit">Jakub Olejnik</p>
-      </a>
-      <a
-        href="https://github.com/MeramQ"
-        rel="noreferrer"
-        target="_blank"
-        className="flex flex-col gap-3"
-      >
-        <FontAwesomeIcon
-          className="w-10 h-10 mx-3 text-zinc-500"
-          icon={faGithub}
-        />
-        <p className="h-fit">MeramQ</p>
-      </a>
+    <div className="flex justify-evenly w-full max-w-2xl gap-5 text-center">
+      <SingleIcon githubUrl="https://github.com/JakubOlejnik2007" githubName="Jakub Olejnik" />
+      <SingleIcon githubUrl="https://github.com/JakubOlejnik2007/gwent-on-chain" githubName="Repozytorium projektu" />
+      <SingleIcon githubUrl="https://github.com/MeramQ" githubName="MeramQ"/>
     </div>
   );
 }
