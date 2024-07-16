07bb2f4676c55d2c90e2d0bbe07e0b7b742ff8b0
JakubOlejnik2007 <jacobole2000@gmail.com>
2024-07-16 23:17:31 +0200

    Added icon for app and changed layout of rules

--- packages/frontend/src/components/profile/GwentRules.tsx
+++ packages/frontend/src/components/profile/GwentRules.tsx
@@ -8,10 +8,8 @@ const GwentRules = () => {
         <div className="w-full max-w-4xl border-zinc-700/50 border-[1px] bg-zinc-900 px-5 py-5 drop-shadow-xl rounded-3xl flex flex-col items-center">
           <div className="flex flex-col items-center w-full gap-10 py-8 md:px-8">
             <div className="text-2xl font-bold">Podstawowe zasady gry</div>
-            <div className="flex flex-col items-center gap-5">
-              <div className="grid grid-cols-2 gap-5 md:grid-cols-4">
+            <div className="flex items-stretch justify-center flex-wrap gap-5">
                 {gwentRules.map((rule, index) => <RuleCard key={index} {...rule} />)}
-              </div>
             </div>
           </div>
         </div>
--- packages/frontend/src/components/profile/RuleCard.tsx
+++ packages/frontend/src/components/profile/RuleCard.tsx
@@ -3,13 +3,13 @@ import { RuleCardProps } from './gwentRules.helper';
 
 const RuleCard = ({ruleNum, abstract, content}: RuleCardProps) => {
     return (
-        <div className="flex flex-col items-center gap-5 p-5 border w-44 rounded-xl border-zinc-700 bg-zinc-800">
+        <div className="flex flex-col items-center gap-5 p-5 border w-44 rounded-xl border-zinc-700 bg-zinc-800 hover:scale-105">
             <p className="items-center justify-center hidden w-8 h-8 text-xl font-bold rounded-full md:flex bg-zinc-300 text-zinc-800">
               {ruleNum}
             </p>
-            <p className="w-32 font-bold text-center overflow-clip whitespace-nowrap overflow-ellipsis">
+            <h2 className="w-32 font-bold text-center overflow-clip whitespace-nowrap overflow-ellipsis">
                 {abstract}
-            </p>
+            </h2>
             <p className="w-32 text-center overflow-clip overflow-ellipsis">
                 {content}
             </p>
